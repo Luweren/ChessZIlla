96c7ceb3038f78885b6098e6ba9c77f2775d33d6
Luweren <luwerenmade@gmail.com>
2023-06-22 21:48:38 +0200

    attempts were made

--- Main.py
+++ Main.py
@@ -1,91 +1,10 @@
-
-size = 64
-
-# Define constants for each square on the board
-A1, B1, C1, D1, E1, F1, G1, H1 = range(8)
-A2, B2, C2, D2, E2, F2, G2, H2 = range(8, 16)
-A3, B3, C3, D3, E3, F3, G3, H3 = range(16, 24)
-A4, B4, C4, D4, E4, F4, G4, H4 = range(24, 32)
-A5, B5, C5, D5, E5, F5, G5, H5 = range(32, 40)
-A6, B6, C6, D6, E6, F6, G6, H6 = range(40, 48)
-A7, B7, C7, D7, E7, F7, G7, H7 = range(48, 56)
-A8, B8, C8, D8, E8, F8, G8, H8 = range(56, 64)
-
-# Define bitboard representations for each square
-squares = {
-    'a1': 1 << A1, 'b1': 1 << B1, 'c1': 1 << C1, 'd1': 1 << D1,
-    'e1': 1 << E1, 'f1': 1 << F1, 'g1': 1 << G1, 'h1': 1 << H1,
-    'a2': 1 << A2, 'b2': 1 << B2, 'c2': 1 << C2, 'd2': 1 << D2,
-    'e2': 1 << E2, 'f2': 1 << F2, 'g2': 1 << G2, 'h2': 1 << H2,
-    'a3': 1 << A3, 'b3': 1 << B3, 'c3': 1 << C3, 'd3': 1 << D3,
-    'e3': 1 << E3, 'f3': 1 << F3, 'g3': 1 << G3, 'h3': 1 << H3,
-    'a4': 1 << A4, 'b4': 1 << B4, 'c4': 1 << C4, 'd4': 1 << D4,
-    'e4': 1 << E4, 'f4': 1 << F4, 'g4': 1 << G4, 'h4': 1 << H4,
-    'a5': 1 << A5, 'b5': 1 << B5, 'c5': 1 << C5, 'd5': 1 << D5,
-    'e5': 1 << E5, 'f5': 1 << F5, 'g5': 1 << G5, 'h5': 1 << H5,
-    'a6': 1 << A6, 'b6': 1 << B6, 'c6': 1 << C6, 'd6': 1 << D6,
-    'e6': 1 << E6, 'f6': 1 << F6, 'g6': 1 << G6, 'h6': 1 << H6,
-    'a7': 1 << A7, 'b7': 1 << B7, 'c7': 1 << C7, 'd7': 1 << D7,
-    'e7': 1 << E7, 'f7': 1 << F7, 'g7': 1 << G7, 'h7': 1 << H7,
-    'a8': 1 << A8, 'b8': 1 << B8, 'c8': 1 << C8, 'd8': 1 << D8,
-    'e8': 1 << E8, 'f8': 1 << F8, 'g8': 1 << G8, 'h8': 1 << H8
-}
-
-# Define piece bitboards
-EMPTY = 0
-PAWN = 'pawn'
-KNIGHT = 'knight'
-BISHOP = 'bishop'
-ROOK = 'rook'
-QUEEN = 'queen'
-KING = 'king'
-
-white_pieces = {
-    PAWN: 0,
-    KNIGHT: 0,
-    BISHOP: 0,
-    ROOK: 0,
-    QUEEN: 0,
-    KING: 0
-}
-
-black_pieces = {
-    PAWN: 0,
-    KNIGHT: 0,
-    BISHOP: 0,
-    ROOK: 0,
-    QUEEN: 0,
-    KING: 0
-}
-
-# Set initial piece positions
-white_pieces[PAWN] = squares['a2'] | squares['b2'] | squares['c2'] | squares['d2'] | squares['e2'] | squares['f2'] | squares['g2'] | squares['h2']
-white_pieces[KNIGHT] = squares['b1'] | squares['g1']
-white_pieces[BISHOP] = squares['c1'] | squares['f1']
-white_pieces[ROOK] = squares['a1'] | squares['h1']
-white_pieces[QUEEN] = squares['d1']
-white_pieces[KING] = squares['e1']
-
-black_pieces[PAWN] = squares['a7'] | squares['b7'] | squares['c7'] | squares['d7'] | squares['e7'] | squares['f7'] | squares['g7'] | squares['h7']
-black_pieces[KNIGHT] = squares['b8'] | squares['g8']
-black_pieces[BISHOP] = squares['c8'] | squares['f8']
-black_pieces[ROOK] = squares['a8'] | squares['h8']
-black_pieces[QUEEN] = squares['d8']
-black_pieces[KING] = squares['e8']
-
-# Define functions to manipulate bitboards
-def empty_bitboard():
-    return 0
-
-def set_bit(bitboard, square):
-    return bitboard | squares[square]
-
-def clear_bit(bitboard, square):
-    return bitboard & ~squares[square]
-
-def is_square_set(bitboard, square):
-    return bool(bitboard & squares[square])
-
+def shift(bitboard, value):
+    if value < 0:
+        # Convert negative left value to positive right value
+        value = -value
+        return bitboard >> value
+    else:
+        return bitboard << value
 
 class BitboardChess:
     NUM_SQUARES = 64
@@ -231,22 +150,19 @@ class BitboardChess:
             en_passant_rank = 4
 
 
-        if direction is 1:
-            if to_bb & (from_bb << 8):  # Single move forward
-                return True
-        if direction is -1:
-            if to_bb & (from_bb >> 8):  # Single move forward
+        if to_bb & shift(from_bb , 8*direction):  # Single move forward
                 return True
 
-        elif from_square[1] == str(starting_rank) and to_bb & (from_bb << 16 * direction):  # Double move forward
+
+        elif from_square[1] == str(starting_rank) and to_bb & shift(from_bb , 16 * direction):  # Double move forward
             return True
-        elif (to_bb & (from_bb << 7 * direction)) and (to_square[0] != 'h'):  # Capture to the left
+        elif (to_bb & shift(from_bb , 7 * direction)) and (to_square[0] != 'h'):  # Capture to the left
             return True
-        elif (to_bb & (from_bb << 9 * direction)) and (to_square[0] != 'a'):  # Capture to the right
+        elif (to_bb & shift(from_bb , 9 * direction)) and (to_square[0] != 'a'):  # Capture to the right
             return True
-        elif (to_bb & (from_bb << 7 * direction)) and (to_square[1] == str(en_passant_rank)):
+        elif (to_bb & shift(from_bb , 7 * direction)) and (to_square[1] == str(en_passant_rank)):
             return True
-        elif (to_bb & (from_bb << 9 * direction)) and (to_square[1] == str(en_passant_rank)):
+        elif (to_bb & shift(from_bb , 9 * direction)) and (to_square[1] == str(en_passant_rank)):
             return True
 
         return False
