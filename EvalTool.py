9297afdc25886ddf80b3b1e0202d9f613f3d97dd
Garic152 <erictheiler15@gmail.com>
2023-07-13 09:32:22 +0200

    weird error

--- EvalTool.py
+++ EvalTool.py
@@ -1,26 +1,31 @@
 import Main
 import evaluate
 import alpha_beta
-
-def main_loop(bitboard:Main.BitboardChess, board_fen,depth):
-    bitboard.load_from_fen(board_fen)
-    checkmate = False
-    remi = False
-    boards = []
-    while not checkmate:
-        best_move = alpha_beta.minmax_get_best_move(bitboard,depth)
-        bitboard.make_move(best_move[1][0],best_move[1][1])
-        bitboard.print_board()
-        print("Eval für Spieler: " + chess.current_player)
-        print(evaluate.evaluate_board(chess, chess.current_player))
-        inp = input(" ")
-
+import time
 
 chess = Main.BitboardChess()
-print("TEST EVAL")
-fen = 'rnbqkbnr/pppppppp/8/8/8/3p4/PPP1PPPP/RNBQKBNR w - - 0 1'
-chess.load_from_fen(fen)
+
+fen = '8/8/8/8/8/8/8/K2b4 w - - 0 1'
+fen2 = '8/8/8/8/8/8/b7/Kb6 w - - 0 1'
+start = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+chess.load_from_fen(fen2)
+
 chess.print_board()
-print(chess.generate_all_player_moves())
-print(alpha_beta.minmax_get_best_move(chess,5))
-print(evaluate.evaluate_board(chess,chess.current_player))
+
+shannon = [20, 400, 8902, 197281, 4865609]\
+
+chess.make_move('a1', 'b1')
+
+chess.print_board()
+
+#for i in range(1):
+#    timer = time.time()
+#
+#    score, tree_size = alpha_beta.minmax_get_best_move(chess, 3)
+#    chess.make_move(score[1][0], score[1][1])
+#
+#    print("Best move:", score[1])
+#    print("Time:" + str(time.time() - timer))
+#
+#    chess.print_board()
+
--- alpha_beta.py
+++ alpha_beta.py
@@ -5,20 +5,29 @@ import copy
 #https://www.chessprogramming.org/Minimax
 #https://www.chessprogramming.org/Alpha-Beta
 
+tree_size = 0
+
 #invocation of alphaBetaMax with initial values can be written in Main.py
 def minmax_get_best_move(bitboard:main.BitboardChess, depthleft):
 
     score = alphaBetaMax((float('-inf'), ('a2','a3')), (float('inf'), ('a7','a6')), depthleft, bitboard)
-    return score
+    return score, tree_size
 
 def alphaBetaMax(alpha, beta, depthleft, bitboard:main.BitboardChess):
+    global tree_size
+
     if depthleft == 0:
+        tree_size += 1
         return (evaluate.evaluate_board(bitboard,'white'),alpha[1])  #'white' should be changed to a player that will be defined and given to the alphaBetaMax in the playing game function
     boardcopy = pickle.dumps(bitboard)
     moves = bitboard.generate_all_player_moves()
+
     for move in moves:
         temp_bitboard = pickle.loads(boardcopy)    #hope it works without deepcopy
-        temp_bitboard.make_move(move[0],move[1])
+
+        if not temp_bitboard.make_move(move[0], move[1]):
+            continue
+
         score = alphaBetaMin(alpha, beta, depthleft-1, temp_bitboard)
         #alpha := (alpha_eval, alpha_move)
         #beta := (beta_eval, beta_move)
@@ -30,20 +39,27 @@ def alphaBetaMax(alpha, beta, depthleft, bitboard:main.BitboardChess):
     return (alpha[0],alpha[1])
 
 def alphaBetaMin(alpha, beta, depthleft, bitboard:main.BitboardChess):
+    global tree_size
+
     if depthleft == 0:
+        tree_size += 1
         return (evaluate.evaluate_board(bitboard,'white'), beta[1])
+    
     boardcopy = pickle.dumps(bitboard)
     moves = bitboard.generate_all_player_moves()
+
     for move in moves:
         temp_bitboard = pickle.loads(boardcopy)       #hope it works without deepcopy
-        temp_bitboard.make_move(move[0], move[1])
+
+        if not temp_bitboard.make_move(move[0], move[1]):
+            continue
+
         score = alphaBetaMax(alpha, beta, depthleft-1, temp_bitboard)
         #alpha := (alpha_eval, alpha_move)
         #beta := (beta_eval, beta_move)
+
         if score[0] <= alpha[0]:
             return (alpha[0],alpha[1])
         if score[0] < beta[0]:
             beta = (score[0], (move[0], move[1]))
     return (beta[0],beta[1])
-
-
